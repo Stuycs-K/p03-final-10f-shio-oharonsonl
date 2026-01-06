b7c141bec93bf8703e4ebc467ee66c428091dbfc
[200~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~ <2008owenshi@gmail.com>
2026-01-06 15:23:11 -0500

    add in makefile and netowkring.c code from previous lab. also init'd client.c and server.c files

--- makefile
+++ makefile
@@ -1,1 +1,19 @@
-
+.PHONY: server-run client-run server-compile client-compile clean
+server-run: server
+	@./server
+client-run: client
+	@./client
+server-compile server: server.o networking.o
+	@gcc -o server server.o
+client-compile: client.o networking.o
+	@gcc -o client client.o
+networking.o: networking.c networking.h
+	@gcc -c networking.c
+client.o: client.c networking.h
+	@gcc -c client.c
+server.o: server.c networking.h
+	@gcc -c server.c
+clean:
+	@rm -r *.o
+	@rm -r server
+	@rm -r client
---
+++ networking.c
@@ -0,0 +1,51 @@
+#include <sys/socket.h>
+
+int server_setup() {
+  struct addrinfo *hints, *results;
+  hints = malloc(sizeof(struct addrinfo));
+  hints->ai_family = AF_INET;
+  hints->ai_socktype = SOCK_STREAM;
+  hints->ai_flags = AI_PASSIVE;
+  getaddrinfo(NULL, "43267", hints, &results);
+
+  int server_socket =
+      socket(results->ai_family, results->ai_socktype, results->ai_protocol);
+
+  int yes = 1;
+  int sockOpt = setsockopt(clientd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
+  err(sockOpt, "sockopt  error");
+
+  bind(server_socket, results->ai_addr, results->ai_addrlen);
+  listen(server_socket, 1);
+  free(hints);
+  freeaddrinfo(results);
+
+  return server_socket;
+}
+
+int server_tcp_handshake(int listen_socket) {
+  return accept(listen_socket, NULL, NULL);
+}
+
+int client_tcp_handshape(char *server_addr) {
+  struct addrinfo *hints, *results;
+  hints = malloc(sizeof(struct addrinfo));
+  hints->ai_family = AF_INET;
+  hints->ai_socktype = SOCK_STREAM;
+  getaddrinfo(server_address, "43267", hints, &results);
+
+  int server_socket = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
+  connect(server_socket, results->ai_addr, results->ai_addrlen);
+
+  free(hints);
+  freeaddrinfo(results);
+
+  return server_socket;
+}
+
+void err(int i, char *msg) {
+  if (i < 0) {
+    printf("Error: %s - %s\n", message, strerror(errno));
+    exit(1);
+  }
+}
