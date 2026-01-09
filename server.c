8674542db5a3ae8e8bba30b3269b70fa5c29d826
[200~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~ <2008owenshi@gmail.com>
2026-01-09 15:30:50 -0500

    semaphores and waiting works

--- server.c
+++ server.c
@@ -14,11 +14,9 @@ void subserver_logic(int client_socket, char *id) {
   send(client_socket, id, sizeof(id), 0); // send the client's id
 
   int sema = semget(KEY, 1, 0);
-  printf("%d\n", semctl(sema, 0, GETVAL));
-  while (semctl(sema, 0, GETVAL) == 1)
-    ; // wait until 8 clients
+  while (semctl(sema, 0, GETVAL) == 1); // wait until 8 clients
 
-  printf("GAME START.");
+  send(client_socket, "1", sizeof(char), 0); // tell clients game has started
 
   exit(0); // Must exit the fork
 }
@@ -26,36 +24,31 @@ void subserver_logic(int client_socket, char *id) {
 // remove shared memory on quit
 static void sighandler(int signo) {
   if (signo == SIGINT) {
-    int shmid = shmget(KEY, sizeof(int), IPC_CREAT | 0640);
+    int shmid = shmget(KEY, sizeof(int), 0);
+    int sema = semget(KEY, 1, 0);
+
     shmctl(shmid, IPC_RMID, 0);
+    semctl(sema, IPC_RMID, 0);
     exit(1);
   }
 }
 
 int main() {
   signal(SIGINT, sighandler);
-
   int server_socket = server_setup();
 
-  int mem_id = shmget(KEY, sizeof(int), IPC_CREAT | 0664);
-  int sema = semget(KEY, 1, IPC_CREAT | 0664);
+  int mem_id = shmget(KEY, sizeof(char), IPC_CREAT | 0664);
+  int sema = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0664);
 
   union semun us;
   us.val = 1;
-  int i = semctl(sema, 0, SETVAL, us);
-  if (i == -1) {
-    perror("bruh");
-    exit(1);
-  }
+  semctl(sema, 0, SETVAL, us);
 
-  printf("froms erver: %d\n", semctl(sema, 0, GETVAL));
-
-  int *clients = shmat(mem_id, 0, 0);
-
-  while (*clients < 8) {
+  int clients = 0;
+  while (clients < 8) {
     int client_socket = server_tcp_handshake(server_socket);
     printf("A CLIENT HAS BEEN CONNECTED\n");
-    (*clients)++;
+    clients++;
 
     int subserver = fork();
     if (subserver == -1) {
@@ -65,7 +58,7 @@ int main() {
 
     else if (subserver == 0) {
       char buf[4];
-      sprintf(buf, "%d", *clients);
+      sprintf(buf, "%d", clients);
       subserver_logic(client_socket, buf);
     }
   }
@@ -74,4 +67,5 @@ int main() {
 
   // remove later I think
   shmctl(mem_id, IPC_RMID, 0); // remove the segment
+  semctl(sema, IPC_RMID, 0);
 }
