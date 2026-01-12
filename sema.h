6a9f19897eac63c670330faaa019157396674016
[200~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~ <2008owenshi@gmail.com>
2026-01-12 15:26:25 -0500

    add 4 boards

--- sema.h
+++ sema.h
@@ -1,14 +1,18 @@
 #ifndef SEMA_H
 #define SEMA_H
 
-#define KEY 234987271
+#define STATES_KEY 234987271
+#define BOARD_ONE 233949049
+#define BOARD_TWO 22949049
+#define BOARD_THREE 26949049
+#define BOARD_FOUR 78949049
 
-// union semun {
-//   int val;
-//   struct semid_ds *buf;
-//   unsigned short *array;
-//   struct seminfo *__buf;
-// };
+union semun {
+  int val;
+  struct semid_ds *buf;
+  unsigned short *array;
+  struct seminfo *__buf;
+};
 
 void decsem(int sema);
 void incsem(int sema);
